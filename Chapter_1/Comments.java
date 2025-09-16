9260606ea4b86276215a756b8bc3ff1c0923c139
edmar122 <imwhiteyt02@gmail.com>
2025-09-16 08:57:20 +0800

    Add files via upload

---
+++ Chapter_1/Comments.java
@@ -0,0 +1,12 @@
+public class Comments {
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Single-line comment example.");
+         
+        System.out.println("Multi-line comment example.");    
+        
+        System.out.println("Documentation comment example.");
+    }
+}
---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,8 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        System.out.println("Kung bibitaw nang mahinahon");
+        System.out.println("Ako ba'y lulubayan ng ating");
+        System.out.println("Mga kahapon na 'di na kayang");
+        System.out.println("Ayusin ng lambing");
+    }
+}
